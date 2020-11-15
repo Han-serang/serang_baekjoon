7c578058c3950f12ff88d19bafcebeb0f7eb0019
serang <rang_se@naver.com>
2020-11-15 17:32:49 +0900

    no message

---
+++ 3079/main.py
@@ -0,0 +1,21 @@
+#2주차 백준 문제풀이 3079 입국심사
+
+import sys
+
+n,m = map(int, sys.stdin.readline().split())
+t = [int(sys.stdin.readline()) for _ in range(n)]
+
+left = min(t)
+answer = right = max(t) * m
+
+while left <= right:
+    total = 0
+    mid = (left + right) // 2
+    for i in range(n):
+        total += mid // t[i]
+    if total >= m:
+        right = mid - 1
+        answer = min(answer,mid)
+    else :
+        left = mid + 1
+print(answer)
