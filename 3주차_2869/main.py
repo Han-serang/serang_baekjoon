e0f82ed1fe9e55dbe92a376d5e2782a546d30a38
serang <rang_se@naver.com>
2020-11-29 16:33:11 +0900

    3주차 문제풀이 완료

--- 3주차_2869/main.py
+++ 3주차_2869/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #3주차 백준 문제풀이 2869 입국심사
 
 A, B, V = map(int, input().split())
@@ -9,5 +8,3 @@ if high % (A-B) == 0:
 else :
     first = int(high/(A-B)+1)
 print(first+1)
-=======
->>>>>>> parent of 43d671a (1주차 문제풀이 완료)
